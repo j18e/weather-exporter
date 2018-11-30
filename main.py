5d2c38caf0d6c4b8764c6e636b0e63c03d0a2d20
James MacFarlane <james@macfarlanetech.com>
2018-11-30 16:30:07 +0100

    wip

--- main.py
+++ main.py
@@ -12,23 +12,23 @@ Options:
     --api-key <api_key>              Dark Sky API key
     --coordinates <coordinates>     Coordinates for forecast (49.38383,11.38383)
     --refresh-interval <interval>   Seconds to wait between fetching weather data
-    --port <port>                   TCP port on which to expose Prometheus metrics
 """
 
-from prometheus_client import start_http_server, Gauge
 from datetime import datetime, timedelta
 from docopt import docopt
 from json import loads
 from requests import get
 from time import sleep
-from datetime import datetime
+
+from prometheus_client import start_http_server
+from prometheus_client.core import REGISTRY, GaugeMetricFamily
 
 def log_message(message):
     time_format = "%Y-%m-%dT%H:%M:%S"
     timestamp = datetime.now()
     print(timestamp.strftime(time_format), message)
 
-def get_forecast(args):
+def get_forecast():
     if args['test']:
         with open(args['<filename>'], 'r') as stream:
             data = loads(stream.read())
@@ -92,41 +92,25 @@ def build_daily(entries):
         })
     return results
 
-def purge_labels(metric, old, new):
-    new_labels = [e['day'] + e['time'] for e in new]
-    for entry in old:
-        if entry['day'] + entry['time'] not in new_labels:
-            metric.remove(entry['day'], entry['time'])
+class DarkSkyCollector(object):
+    def collect(self):
+        data = get_forecast(args)
 
-def main(args):
-    if args['--refresh-interval']:
-        refresh_interval = int(args['--refresh-interval'])
-    else:
-        refresh_interval = 120
-    if args['--port']:
-        metrics_port = int(args['--port'])
-    else:
-        metrics_port = 8080
-    start_http_server(metrics_port)
-    TEMP = Gauge('forecast_temperature', 'forecasted temperature in degrees celcius', ['day', 'time'])
-    old_hourly = []
-    while True:
-        forecast_data = get_forecast(args)
+        TEMP = GaugeMetricFamily('forecast_temperature',
+            'forecasted temperature', labels=['day', 'time'])
+        TEMP.add_metric(['today', 'now'],
+            data['currently']['apparentTemperature'])
 
-#        hourly = build_hourly(forecast_data['hourly']['data'])
-#        purge_labels(TEMP, old_hourly, hourly)
-#        TEMP.labels('today', 'now').set(forecast_data['currently']['apparentTemperature'])
-#        for entry in hourly:
-#            TEMP.labels(entry['day'], entry['time']).set(entry['temp'])
-#        old_hourly = hourly
+        for hour in build_hourly(data['hourly']['data']):
+            TEMP.add_metric([hour['day'], hour['time']], hour['temp'])
 
-        daily = build_daily(forecast_data['daily']['data'])
-        for e in daily:
-            print(e)
+        yield TEMP
 
-        sleep(refresh_interval)
 
 if __name__ == '__main__':
     args = docopt(__doc__, options_first=True)
-    main(args)
+    REGISTRY.register(DarkSkyCollector())
+    start_http_server(8080)
+    while True:
+        sleep(1)
 
